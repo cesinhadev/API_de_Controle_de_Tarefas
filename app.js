aaa11263742dc3ad92a0bc59821714ccc2dd92c0
Cesar Augusto <cesinhalius@hotmail.com>
2025-01-16 16:40:30 -0300

    Adicionando conexão com o MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Imports  */
 require("dotenv").config();
-const express = require("express"); 
+const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -11,4 +11,16 @@ app.get("/", (req, res) => {
 	res.status(200).json({ msg: " Bem vindo a nossa API" });
 });
 
-app.listen(3000);
+//Cretenciais
+const dbUser = process.env.DB_USER 
+const dbPassword = process.env.DB_PASS 
+
+mongoose
+	.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hawmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+	.then(() => {
+        app.listen(3000);
+        console.log('Conectou ao banco!')
+    })
+	.catch((err) => console.log(err));
+
+
