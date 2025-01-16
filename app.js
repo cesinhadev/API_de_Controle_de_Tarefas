74a646aab2c2efd9d4340290747241561161a939
Cesar Augusto <cesinhalius@hotmail.com>
2025-01-16 16:26:57 -0300

    Adicionando servidor

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-require('dotenv').config()
+/* Imports  */
+require("dotenv").config();
+const express = require("express"); 
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.status(200).json({ msg: " Bem vindo a nossa API" });
+});
+
+app.listen(3000);
