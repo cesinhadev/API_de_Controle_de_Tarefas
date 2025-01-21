5018b9a07cf6a399c3964fcdc6ed4704441bdf06
Cesar Augusto <cesinhalius@hotmail.com>
2025-01-21 16:16:16 -0300

    Refatoranto o arquivo principal

--- app.js
+++ app.js
@@ -1,79 +1,58 @@
-/* Imports  */
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const { errorHandler } = require("./middlewares/errorHandler");
 
-// Models
-const User = require("./models/User");
+// Importação das rotas
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+
 
 const app = express();
-// Config Json responde
+
+// Middlewares
 app.use(express.json());
 
+// Configurações
+const PORT = process.env.PORT
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hawmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Rotas
 app.get("/", (req, res) => {
-	res.status(200).json({ msg: " Bem vindo a nossa API" });
+    res.status(200).json({ msg: "Bem-vindo à nossa API" });
 });
 
-//Register user
-app.post("/auth/register", async (req, res) => {
-	const { name, email, password, confirmpassword } = req.body;
+// Registra as rotas
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
-	// validação
-	if (!name) {
-		return res.status(422).json({ msg: "O nome é obrigatório" });
-	}
-	if (!email) {
-		return res.status(422).json({ msg: "O email é obrigatório" });
-	}
-	if (!password) {
-		return res.status(422).json({ msg: "A senha é obrigatório" });
-	}
-	if (password !== confirmpassword) {
-		return res.status(422).json({ msg: "A senha não conferem!!" });
-	}
 
-	// if user exist
+// Middleware de tratamento de erros
+app.use(errorHandler);
 
-	const userExist = await User.findOne({ email: email });
+// Conexão com o banco de dados
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log("Conectado ao banco de dados com sucesso!");
+    } catch (error) {
+        console.error("Erro ao conectar ao banco de dados:", error.message);
+        process.exit(1);
+    }
+};
 
-	if (userExist) {
-		return res.status(422).json({ msg: "Por favor , utilize outro e-mail" });
-	}
+// Inicialização do servidor
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Erro ao iniciar o servidor:", error.message);
+        process.exit(1);
+    }
+};
 
-	//create password
-	const salt = await bcrypt.genSalt(12);
-	const passwordHash = await bcrypt.hash(password, salt);
+startServer();
 
-	//create user
-	const user = new User({
-		name,
-		email,
-		password: passwordHash,
-	});
-	try {
-		await user.save();
-		res.status(201).json({ msg: "Usuario criado com sucesso" });
-	} catch (error) {
-		console.log(error);
-
-		res
-			.status(500)
-			.json({ msg: "Aconteceu um erro no servidor, tente mais tarde!!" });
-	}
-});
-
-//Cretenciais
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASS;
-
-mongoose
-	.connect(
-		`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hawmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-	)
-	.then(() => {
-		app.listen(3000);
-		console.log("Conectou ao banco!");
-	})
-	.catch((err) => console.log(err));
